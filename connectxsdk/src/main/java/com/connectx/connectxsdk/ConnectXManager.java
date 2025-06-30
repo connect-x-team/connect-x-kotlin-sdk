13f17fbbd897ea3953e58ed0c8d4be57176c92f6
Surasak <surasak@mintel.tech>
2025-06-30 13:24:29 +0700

    increase version

--- connectxsdk/src/main/java/com/connectx/connectxsdk/ConnectXManager.java
+++ connectxsdk/src/main/java/com/connectx/connectxsdk/ConnectXManager.java
@@ -227,7 +227,7 @@ public class ConnectXManager {
         clientData.put("cx_networkType", getNetworkType(userContext));
         clientData.put("cx_appVersion", getAppVersion());
         clientData.put("cx_appBuild", getAppBuildVersion());
-        clientData.put("cx_libraryVersion", "1.0.11");
+        clientData.put("cx_libraryVersion", "1.0.12");
         clientData.put("cx_libraryPlatform", "Android");
         clientData.put("cx_fingerprint", getDeviceId(userContext));
         clientData.put("cx_deviceId", getDeviceId(userContext));
